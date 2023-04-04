b3fbb9c50a8155b3431fbe32397e6f9ab909dac6
wangminghao30 <129815680+wangminghao30@users.noreply.github.com>
2023-04-04 13:16:07 +0900

    Add files via upload

---
+++ 0328/p115_6.py
@@ -0,0 +1,22 @@
+'''
+2023.03.28
+왕명호
+47과 18의 가감승제 구하는 프로그램
+'''
+su1=47
+su2=18
+
+#res1=num1+num2 
+#res2=num1-num2 
+#res3=num1*num2 
+#res4=num1/num2 
+
+#print("num1+num2=",res1)
+#print("num1-num2=",res2)
+#print("num1*num2=",res3)
+#print("num1/num2=",res4)
+
+print(su1,"과",su2,"의 합은 :",su1+su2)
+print(su1,"과",su2,"의 차은 :",su1-su2)
+print(su1,"과",su2,"의 곱은 :",su1*su2)
+print(su1,"과",su2,"의 나눗셈은 :",su1/su2)
---
+++ 0328/p116_12.py
@@ -0,0 +1,14 @@
+'''
+2023.03.28
+왕명호
+
+'''
+
+radius1=23
+radius2=34
+radius3=13
+area=0
+
+area=((radius1+radius2)*radius3)/2
+
+print("사다리꼴 넓이:",area)
